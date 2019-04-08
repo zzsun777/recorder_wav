f8ae2011542ee6c5b789ef53df6cd81bfb606fb4
zzsun777 <75812512@qq.com>
2019-04-08 15:37:51 +0800

    1

--- lib/recorder_wav.dart
+++ lib/recorder_wav.dart
@@ -1,19 +1,17 @@
-
 import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/services.dart';
 
 class RecorderWav {
-  static const MethodChannel _channel =
-      const MethodChannel('recorder_wav');
+  static const MethodChannel _channel = const MethodChannel('recorder_wav');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static Future<String> StopRecorder() async {
+  static Future<String> stopRecorder() async {
     var file = await _channel.invokeMethod("stopRecorder");
     return file;
   }
@@ -21,6 +19,10 @@ class RecorderWav {
   static startRecorder() {
     _channel.invokeMethod("startRecorder");
   }
+
+  // static playRecorder() {
+  //   _channel.invokeMethod("playRecorder");
+  // }
 
   static removeRecorderFile(String fileName) {
     _channel.invokeMethod("removeFile", {'file': fileName});
